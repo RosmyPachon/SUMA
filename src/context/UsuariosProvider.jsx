0e24bf94372fa60bb59db68390f40794de02d5f9
RosmyPachon <rosmypachon@gmail.com>
2023-10-11 17:55:14 -0500

    feat(usuarios):Validaciones

--- src/context/UsuariosProvider.jsx
+++ src/context/UsuariosProvider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState, createContext, useRef } from "react";
 import conexionCliente from "../config/ConexionCliente";
 import { useLocation } from "react-router-dom";
 
@@ -71,7 +71,7 @@ const UsuariosProvider = ({ children }) => {
     clave: "",
     claverepetida: "",
   });
-
+  
   const [errors, setErrors] = useState({
     nombre: '',
     usuario: '',
@@ -79,17 +79,15 @@ const UsuariosProvider = ({ children }) => {
     clave: '',
     claverepetida: '',
   });
-
+  
   const [perfilesAgg, setPerfilesAgg] = useState([]);
   const [modulosAgg, setModulosAgg] = useState([]);
   const [permisosAgg, setPermisosAgg] = useState([]);
 
-  const [validacionesExitosas, setValidacionesExitosas] = useState(true);
 
-
-  // const handleChangeUsuario = (e) => {
-  //   setUsuariosAgg({ ...UsuariosAgg, [e.target.name]: e.target.value });
-  // };
+  const handleChangeUsuario = (e) => {
+    setUsuariosAgg({ ...UsuariosAgg, [e.target.name]: e.target.value });
+  };
 
   useEffect(() => {
     const getUsuarios = async () => {
@@ -170,58 +168,12 @@ const UsuariosProvider = ({ children }) => {
       // Realiza la solicitud POST a la API para guardar la información del usuario
       const response = await conexionCliente.post("/usuarios", formData, config);
       console.log("Información guardada con éxito:", response);
+      // toast.current.show({severity:'success', summary: 'Success', detail:'Registro guardado con exito', life: 3000});
       return response.data; // Puedes devolver los datos guardados si es necesario
     } catch (error) {
       console.error("Error al guardar la información:", error);
       throw error; // Puedes lanzar una excepción en caso de error
     }
-  };
-
-   // -----------------validaciones-----------------------------
-
-   const handleChangeUsuario = (e) => {
-    const { name, value } = e.target;
-    setUsuariosAgg({ ...UsuariosAgg, [name]: value });
-    // setUsuariosAgg({ ...UsuariosAgg, [name]: value });
-    // setUsuariosAgg({ ...UsuariosAgg, [e.target.name]: e.target.value });
-
-    // Realiza validaciones específicas para cada campo
-    if (name === 'nombre') {
-      if (value.length < 10 || value.length > 30) {
-        setErrors({ ...errors, [name]: 'Debe tener entre 10 y 30 caracteres' });
-      } else {
-        setErrors({ ...errors, [name]: '' });
-      }
-    }
-
-    if (name === 'usuario') {
-      if (value.length < 5 || value.length > 15) {
-        setErrors({ ...errors, [name]: 'El usuario debe tener entre 5 y 15 caracteres' });
-      } else {
-        setErrors({ ...errors, [name]: '' });
-      }
-    }
-
-    if (name === 'correo') {
-      // Realiza validación de correo electrónico (puedes utilizar una expresión regular)
-      const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-      if (!emailPattern.test(value)) {
-        setErrors({ ...errors, [name]: 'El correo electrónico no es válido' });
-      } else {
-        setErrors({ ...errors, [name]: '' });
-      }
-    }
-
-    // if (name === 'clave' || name === 'claverepetida') {
-    //   if (UsuariosAgg.clave !== UsuariosAgg.claverepetida) {
-    //     setErrors({ ...errors, clave: 'Las contraseñas no coinciden' });
-    //   } else {
-    //     setErrors({ ...errors, clave: '', claverepetida: '' });
-    //   }
-    // }
-
-    
-    
   };
 
 
